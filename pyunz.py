2f662ca4c38b3f9ea320de10e150839ecd359fe2
VampireSH <sherlockya@gmail.com>
2017-03-09 19:09:14 +0800

    test version

--- pyunz.py
+++ pyunz.py
@@ -30,41 +30,64 @@ def untar(file):
         print("You don't have tar")
         return sys.exit(1)
     args = '-xvf'
-    subprocess.Popen([bin('tar'), args, file])
+    subprocess.run([bin('tar'), args, file])
 
 def unzip(file):
     if 'core_perl' in bin('unzip'):
         print("You don't have unzip")
         return sys.exit(1)
     args = '-o'
-    subprocess.Popen([bin('unzip'), args, file])
+    subprocess.run([bin('unzip'), args, file])
 
 def un7z(file):
-    _7z1 = '7z'
-    _7z2 = '7za'
+    _7z = '7z'
+    _7za = '7za'
     args1 = 'x'
     args2 = '-y'
 
-    if 'core_perl' not in bin(_7z1):
-        subprocess.Popen([bin(_7z1), args1, args2, file])
-    elif 'core_perl' not in bin(_7z2):
-        subprocess.Popen([bin(_7z2), args1, args2, file])
+    if 'core_perl' not in bin(_7z):
+        subprocess.run([bin(_7z), args1, args2, file])
+    elif 'core_perl' not in bin(_7za):
+        subprocess.run([bin(_7za), args1, args2, file])
     else:
         print("You don't have 7z or 7za")
         return sys.exit(1)
 
-def pkg_error():
+def compress_tar(type, i_file, o_file):
+    if type == 'tgz':
+        args = 'cvzf'
+        subprocess.run([bin('tar'), args, o_file, i_file])
+
+    elif type == 'txz':
+        args = 'cvJf'
+        subprocess.run([bin('tar'), args, o_file, i_file])
+
+    elif type == 'tbz':
+        args = 'cvjf'
+        subprocess.run([bin('tar'), args, o_file, i_file])
+
+def compress_7z(i_file, o_file):
+    args = 'a'
+    _7z = '7z'
+    _7za = '7za'
+    if 'core_perl' not in bin(_7z):
+        subprocess.run([bin(_7z), args, o_file, i_file])
+
+    elif 'core_perl' not in bin(_7za):
+        subprocess.run([bin(_7za), args, o_file, i_file])
+
+def compress_zip(i_file, o_file):
+    subprocess.run([bin('zip'), o_file, i_file])
+
+def package_error():
     print('Is it a package?')
     return sys.exit(1)
-
-#def not_found():
-#    print('Where is the package?')
-#    return sys.exit(1)
 
 parser = argparse.ArgumentParser(description = 'an extract tool for tgz, zip, 7z...')
 parser.add_argument('-x', '--extract', help = 'extract the package automatically')
 parser.add_argument('-v', '--version',action = 'store_true', help = 'print version')
-parser.add_argument('-c', '--create', choices = ['tgz', 'txz', 'tbz2'], help = 'create a package')
+parser.add_argument('-i', '--input', help = 'create a package')
+parser.add_argument('-t', '--type', choices = ['tgz', 'tbz', 'txz', '7z', 'zip'], help = 'choose the package type')
 parser.add_argument('-o', '--output', help = 'the package name')
 
 
@@ -89,7 +112,23 @@ if args.extract:
         unzip(args.extract)
 
     else:
-        pkg_error()
+        package_error()
 
 if args.version:
     print(VERSION)
+
+
+if args.type == 'tgz':
+    compress_tar(args.type, args.input, args.output)
+
+elif args.type == 'tbz':
+    compress_tar(args.type, args.input, args.output)
+
+elif args.type == 'txz':
+    compress_tar(args.type, args.input, args.output)
+
+elif args.type == '7z':
+    compress_7z(args.input, args.output)
+
+elif args.type == 'zip':
+    compress_zip(args.input, args.output)
