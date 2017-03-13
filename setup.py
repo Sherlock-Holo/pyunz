9703305ca419e862e91f578c1231096749150310
VampireSH <sherlockya@gmail.com>
2017-03-13 20:01:42 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = 'pyunz',
+    license = 'GPL-3.0',
+    version = '0.2.3',
+    url = 'https://github.com/Sherlock-Holo/pyunz',
+    author = 'Sherlock Holo',
+    author_email = 'sherlockya@gmail.com',
+    description = 'an extract wrapper',
+    scripts = ['pyunz.py'],
+    classifiers=[
+        'License :: OSI Approved :: GPL-3.0 License',
+        'Programming Language :: Python :: 3.6',
+        ],
+    entry_points = '''
+        [console_scripts]
+        pyunz = pyunz : cli
+    ''',
+)
